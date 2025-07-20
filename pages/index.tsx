9df82736514171bc4fe7ddb4651d6796c3a22297
MacBook <macbook@192.168.68.116>
2025-07-19 22:44:23 -0600

    Agregar carpeta pages en la raíz y archivo .gitignore

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bienvenido a mi sistema de reservaciones de hotel</h1>
+    </div>
+  );
+};
+
+export default HomePage;
